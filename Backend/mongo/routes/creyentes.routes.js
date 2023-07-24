80a40d4bc74e92b8f10d659976f90ecdc319bed7
cristian212010 <cristiancamilo212010@gmail.com>
2023-07-23 21:07:11 -0500

    feat: :bug: Error consulta creyentes solucionado
    error en el import de las rutas de creyentes

--- Backend/mongo/routes/creyentes.routes.js
+++ Backend/mongo/routes/creyentes.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import {getAll, insertData, deleteData, oneData, updateData} from '../controllers/NivelFormacion.controller.js';
+import {getAll, insertData, deleteData, oneData, updateData} from '../controllers/Creyente.controller.js';
 
 const router = Router();
 
